89461a3915ddeb1978a77927088abaa89c790799
mohammed <msharah96@gmail.com>
2022-10-11 11:18:30 +0100

    apply requested changes to app file by making an error controller

--- app.js
+++ app.js
@@ -5,43 +5,23 @@ const { getTopics } = require("./controllers/get_topics.controller");
 const {
   getArticlesById,
 } = require("./controllers/get-articles-by-id.controller");
+const {
+  handle404,
+  handleCustomErrors,
+  handleInternalErrors,
+  handlePSQLErrors,
+} = require("./controllers/errors.controller");
 
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticlesById);
 
+app.use("/", handle404);
 
-app.use("/", (req, res) => {
-  res.status(404).send({ msg: "not found" });
-});
+app.use(handleCustomErrors);
 
+app.use(handlePSQLErrors);
 
-app.use((err, req, res, next) => {
-  if (err.status === 404 && err.msg) {
-    res.status(404).send(err);
-  }
-
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  if (err.status === 400 && err.msg) {
-    res.status(400).send(err)
-  }
-
-  next(err);
-});
-
-app.use((err, req, res, next)=>{
-  if(err.code === "22P02"){
-  res.status(400).send({msg: "invalid id"})
-      }
-  next(err)
-      })
-
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send({ message: "internal error" });
-});
+app.use(handleInternalErrors);
 
 module.exports = app;
