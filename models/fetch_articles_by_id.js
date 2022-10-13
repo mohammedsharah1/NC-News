21dad5d54c2213fcedeee7120ee4a3247ac7bcf9
mohammed <msharah96@gmail.com>
2022-10-13 14:33:38 +0100

    task 08 complete with tests

--- models/fetch_articles_by_id.js
+++ models/fetch_articles_by_id.js
@@ -2,12 +2,14 @@ const db = require("../db/connection");
 
 exports.fetchArticlesById = (id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id=$1
-    COUNT (comments.article_id) ::INT AS comment_count;`, [id])
+    .query(
+      `SELECT * FROM articles WHERE article_id=$1
+    COUNT (comments.article_id) ::INT AS comment_count;`,
+      [id]
+    )
     .then(({ rows }) => {
       if (rows.length !== 0) {
-        return rows[0]
-
+        return rows[0];
       } else {
         return Promise.reject({
           status: 404,
