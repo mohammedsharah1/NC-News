91ee6b43343df2ec62af3e65be6fc375cd72e96c
mohammed <msharah96@gmail.com>
2022-10-15 16:38:56 +0100

    resolve errors for listen.js

--- listen.js
+++ listen.js
@@ -1,3 +1,8 @@
+const app = require("./app");
+
 const { PORT = 9090 } = process.env;
+app.listen(port, (err) => {
+  if (err) throw err;
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
